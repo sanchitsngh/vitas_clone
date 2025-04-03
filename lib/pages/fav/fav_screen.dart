be2a2db6f5c7d28ba50b4675b3ac658d81cbe242
akshita266 <akshitagupta2601@gmail.com>
2025-04-04 01:50:50 +0530

    resolved akshita's merge conflict

--- lib/pages/fav/fav_screen.dart
+++ lib/pages/fav/fav_screen.dart
@@ -104,7 +104,8 @@ class FilteredItemsScreen extends ConsumerWidget {
                     ),
                   ),
                 ],
-        
+              )
+            )
           );
         },
       ),
